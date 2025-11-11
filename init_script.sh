7ffb04183ce9bf4a2bd411b24b30735c70c88fa9
christiandarvs <christiandarvin4@gmail.com>
2025-11-11 16:53:16 +0800

    add files

--- init_script.sh
+++ init_script.sh
@@ -8,7 +8,7 @@ exec > >(tee /var/log/user-data.log | logger -t user-data -s 2>/dev/console) 2>&
 sudo apt update && sudo apt upgrade -y
 
 # Install Python & dependencies
-sudo apt install -y python3 python3-pip python3-venv git unzip libgl1 libglib2.0-0
+sudo apt install -y python3 python3-pip python3-venv git unzip libgl1 libglib2.0-0 transformers
 
 # Variables
 PROJECT_DIR="/home/ubuntu/imusafe-api"
@@ -27,5 +27,29 @@ source venv/bin/activate
 
 pip install --upgrade pip setuptools wheel
 pip install "fastapi[standard]" uvicorn torch torchvision ultralytics
+
+# Create systemd service
+SERVICE_FILE="/etc/systemd/system/imusafe.service"
+
+sudo bash -c "cat > $SERVICE_FILE" <<EOL
+[Unit]
+Description=ImuSafe FastAPI Service
+After=network.target
+
+[Service]
+User=ubuntu
+WorkingDirectory=$PROJECT_DIR
+ExecStart=$PROJECT_DIR/venv/bin/uvicorn app:app --host 0.0.0.0 --port 8000
+Restart=always
+Environment="YOLO_CONFIG_DIR=/home/ubuntu/.config/Ultralytics"
+
+[Install]
+WantedBy=multi-user.target
+EOL
+
+# Enable & start service
+sudo systemctl daemon-reload
+sudo systemctl enable imusafe
+sudo systemctl start imusafe
 
 echo "----- DONE! -----"
--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,14 @@
 output "public_dns" {
-  description = "Display the Public DNS of the EC2 Instance"
-  value       = "ubuntu@${aws_instance.main.public_dns}"
+  description = "Display the Public DNS of the EC2 Instance (Elastic IP)"
+  value       = aws_eip.main.public_dns
 }
 
 output "for_prediction" {
-  description = "Display the Public DNS of the EC2 Instance"
-  value       = "ubuntu@${aws_instance.main.public_dns}:8000/predict/"
+  description = "Prediction endpoint using Elastic IP DNS"
+  value       = "ubuntu@${aws_eip.main.public_dns}:8000/predict/"
+}
+
+output "for_prediction_ip" {
+  description = "Prediction endpoint using Elastic IP address"
+  value       = "http://${aws_eip.main.public_ip}:8000/predict/"
 }
