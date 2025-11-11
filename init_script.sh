4250fc58bd8c8c962fc55f059bea29697b93c96f
christiandarvs <christiandarvin4@gmail.com>
2025-11-11 22:37:24 +0800

    fix

--- init_script.sh
+++ init_script.sh
@@ -8,7 +8,7 @@ exec > >(tee /var/log/user-data.log | logger -t user-data -s 2>/dev/console) 2>&
 sudo apt update && sudo apt upgrade -y
 
 # Install Python & dependencies
-sudo apt install -y python3 python3-pip python3-venv git unzip libgl1 libglib2.0-0 transformers
+sudo apt install -y python3 python3-pip python3-venv git unzip libgl1 libglib2.0-0 
 
 # Variables
 PROJECT_DIR="/home/ubuntu/imusafe-api"
@@ -18,15 +18,19 @@ mkdir -p "$PROJECT_DIR"
 cd "$PROJECT_DIR"
 
 git clone https://github.com/christiandarvs/imusafe-fastapi.git temp_repo
-mv temp_repo/best.pt temp_repo/app.py "$PROJECT_DIR"
+mv temp_repo/best.pt temp_repo/app.py temp_repo/yolov8m.pt "$PROJECT_DIR"
 rm -rf temp_repo
+
+sudo mkdir -p "$PROJECT_DIR/annotated"
+sudo chown -R ubuntu:ubuntu "$PROJECT_DIR/annotated"
+sudo chmod -R 755 "$PROJECT_DIR/annotated"
 
 # Create venv & install deps
 python3 -m venv venv
 source venv/bin/activate
 
 pip install --upgrade pip setuptools wheel
-pip install "fastapi[standard]" uvicorn torch torchvision ultralytics
+pip install "fastapi[standard]" uvicorn torch torchvision ultralytics transformers[torch]
 
 # Create systemd service
 SERVICE_FILE="/etc/systemd/system/imusafe.service"
